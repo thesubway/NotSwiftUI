12c4392f8f5618ec8eac255ad2b483850dda5405
thesubway <thesubway@gmail.com>
2019-07-02 11:26:17 -0700

    Initial Commit

---
+++ NotSwiftUI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NotSwiftUI
+//
+//  Created by Daniel Hoang on 7/2/19.
+//  Copyright © 2019 Daniel Hoang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
